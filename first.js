c2d89133877354ff7a9de35b39800fe8291f303a
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2025-02-09 21:37:24 +0530

    #update

--- first.js
+++ first.js
@@ -1,4 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var a = 'Hello World';
-console.log(a);
+var Abc = /** @class */ (function () {
+    function Abc() {
+    }
+    Abc.prototype.demo = function () {
+        console.log('hello');
+    };
+    return Abc;
+}());
+var a = new Abc();
+a.demo();
